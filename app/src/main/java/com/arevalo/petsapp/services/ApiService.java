cd7b4f059400b978964f2d5f1c4793f6cf746542
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2019-12-07 23:37:08 -0500

    qrcode

--- app/src/main/java/com/arevalo/petsapp/services/ApiService.java
+++ app/src/main/java/com/arevalo/petsapp/services/ApiService.java
@@ -18,7 +18,7 @@ import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface ApiService {
-    String API_BASE_URL = "http://10.0.2.2:8080";
+    String API_BASE_URL = "http://192.168.1.3:8080";
 
     @POST("/users/")
     Call<User> createUser(@Body User user);
